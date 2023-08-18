5af627b9d8088645b49128bddb1bbcb9d3e324d1
quangpham2803 <108970825+quangpham2803@users.noreply.github.com>
2023-08-18 16:35:13 +0700

    thuc hanh 5

---
+++ ThucHanh5/Bai3/lib/constants.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const String google_api_key = "AIzaSyAG3SSMXPUldPt4ogGe_lwZIFbqDF7KM6A";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
